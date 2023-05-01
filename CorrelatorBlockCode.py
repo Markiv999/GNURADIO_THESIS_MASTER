1e9d2dae00e4874e7a7c5076eca7ecdd0f2cb6a6
Markiv999 <91270826+Markiv999@users.noreply.github.com>
2023-05-01 12:04:05 +0200

    Added tone ranging block and correlator code to gnu radio

---
+++ CorrelatorBlockCode.py
@@ -0,0 +1,43 @@
+import numpy as np
+from gnuradio import gr
+import scipy as sp
+
+class CorrelationDelayEstimator(gr.sync_block):
+    def __init__(self, vectorsize=18750,sample_rate=50000):
+        gr.sync_block.__init__(
+            self,
+            name='CorrelationDelayEstimator',
+            in_sig=[np.complex64, np.complex64],
+            out_sig=[np.float32,np.float32]
+        )
+        self.vectorsize = vectorsize
+        self.range_clock_signal = np.zeros(vectorsize, dtype=np.complex64)
+        self.sequential_signal = np.zeros(vectorsize, dtype=np.complex64)
+        self.i = 0
+        self.max = 0
+        self.sample_delay_count = 0
+        self.correlation_values = np.zeros(vectorsize, dtype=np.complex64)
+        self.sample_rate=sample_rate
+
+    def work(self, input_items, output_items):
+        #build from ground up
+        sequential_signal = input_items[0][0]
+        range_clock_signal = input_items[1][0]
+    
+        if self.i < self.vectorsize:
+            self.range_clock_signal[self.i] = range_clock_signal
+            self.sequential_signal[self.i] = sequential_signal
+            self.i += 1
+            output_items[0][:] = 0
+            return 1
+        
+        self.correlation_values = np.fft.ifft(np.fft.fft(self.range_clock_signal) * np.conj(np.fft.fft(self.sequential_signal)))
+         #self.max = np.amax(np.abs(self.correlation_values))
+        self.sample_delay_count = np.argmax(np.abs(self.correlation_values))
+        delay=(self.vectorsize - float(self.sample_delay_count)) 
+        delay_in_micro_seconds= 10**6 * delay/self.sample_rate
+        output_items[0][:] = delay
+        self.i=0
+        print(self.i)
+       # output_items[0][:] = delay_in_micro_seconds
+        return output_items
