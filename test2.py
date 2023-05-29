c04e943c9f7490d1cc1fd6dcb05edbec9c99c370
Markiv999 <91270826+Markiv999@users.noreply.github.com>
2023-05-29 12:35:43 +0200

    commit before implementing decimator

--- test2.py
+++ test2.py
@@ -27,6 +27,7 @@ from gnuradio.filter import firdes
 import sip
 from gnuradio import analog
 from gnuradio import blocks
+from gnuradio import filter
 from gnuradio import gr
 from gnuradio.fft import window
 import sys
@@ -46,7 +47,7 @@ from gnuradio import qtgui
 
 class test2(gr.top_block, Qt.QWidget):
 
-    def __init__(self, delayvalue):
+    def __init__(self):
         gr.top_block.__init__(self, "Not titled yet", catch_exceptions=True)
         Qt.QWidget.__init__(self)
         self.setWindowTitle("Not titled yet")
@@ -86,7 +87,7 @@ class test2(gr.top_block, Qt.QWidget):
         self.variable_qtgui_chooser_0 = variable_qtgui_chooser_0 = 0
         self.variable_0 = variable_0 = (int(frequency),int(frequency/2),int(frequency/4),int(frequency/8))
         self.samp_rate = samp_rate = SampleRateSlider
-        self.delay = delay = delayvalue
+        self.delay = delay = 0
         self.Noise_Amp = Noise_Amp = 0
 
         ##################################################
@@ -101,10 +102,8 @@ class test2(gr.top_block, Qt.QWidget):
             try:
               try:
                 self.doc.add_next_tick_callback(functools.partial(self.set_delay,val))
-                val=val+2
               except AttributeError:
                 self.set_delay(val)
-                print(val)
             except AttributeError:
               pass
             time.sleep(1.0 / (samp_rate))
@@ -229,6 +228,15 @@ class test2(gr.top_block, Qt.QWidget):
 
         self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_time_sink_x_0_win)
+        self.low_pass_filter_0 = filter.fir_filter_fff(
+            18750,
+            firdes.low_pass(
+                1,
+                samp_rate,
+                1000000000000000,
+                1000000000000000,
+                window.WIN_HAMMING,
+                6.76))
         self.epy_block_1 = epy_block_1.CorrelationDelayEstimator(vectorsize=sum(variable_0), sample_rate=samp_rate)
         self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*1, samp_rate,True)
         self.blocks_stream_mux_0 = blocks.stream_mux(gr.sizeof_gr_complex*1, variable_0)
@@ -276,7 +284,8 @@ class test2(gr.top_block, Qt.QWidget):
         self.connect((self.blocks_stream_mux_0, 0), (self.qtgui_time_sink_x_0, 0))
         self.connect((self.blocks_throttle_0, 0), (self.blocks_stream_mux_0, 0))
         self.connect((self.epy_block_1, 0), (self.blocks_null_sink_2, 0))
-        self.connect((self.epy_block_1, 0), (self.probe1, 0))
+        self.connect((self.epy_block_1, 0), (self.low_pass_filter_0, 0))
+        self.connect((self.low_pass_filter_0, 0), (self.probe1, 0))
 
 
     def closeEvent(self, event):
@@ -337,6 +346,7 @@ class test2(gr.top_block, Qt.QWidget):
         self.analog_sig_source_x_0_0_0_1.set_sampling_freq(self.samp_rate)
         self.blocks_throttle_0.set_sample_rate(self.samp_rate)
         self.epy_block_1.sample_rate = self.samp_rate
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, 1000000000000000, 1000000000000000, window.WIN_HAMMING, 6.76))
         self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
         self.qtgui_time_sink_x_2.set_samp_rate(self.samp_rate)
 
